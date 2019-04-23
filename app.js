218fa5d09b0bdb974a4cd9dc0c6210950797ead8
Ravindra Sharma <rksharmait13@gmail.com>
2019-04-23 19:50:40 +0530

    Add server listen

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ app.get('/',function(req,res){
 app.get('/hello',function(req,res){
     res.send('Hello Heroku!!');
 })
+
+app.listen(process.env.PORT || 5000,function(){
+    console.log('app is runnig on 5000')
+})
