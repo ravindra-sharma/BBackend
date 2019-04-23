1d45ca8955e3aa10bcf7e56f35d37a711d291b1b
Ravindra Sharma <rksharmait13@gmail.com>
2019-04-23 19:58:45 +0530

    change path for index.html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '\\public\\'))
 app.use(cors())
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '\\index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/hello',function(req,res){
